dc49e33a63edd4ff8e90c43f1e1a4eac6cd8acfb
segersniels <segers.n@hotmail.com>
2020-11-11 21:51:37 +0100

    :sparkles: Add commander structure so values can be configured using flags

--- index.ts
+++ index.ts
@@ -4,45 +4,64 @@ import fs from 'fs';
 import Lockfile from 'helpers/lockfile';
 import which from 'which';
 import Duplicacy from 'helpers/duplicacy';
+import * as utils from 'helpers/utils';
+import { program } from 'commander';
+import packageJson from 'package';
 
 const lockfile = new Lockfile(__filename);
 const duplicacy = new Duplicacy();
-const repository = process.argv[2];
 
-const error = (message: string) => {
-  console.error(`[ERROR] ${message}`);
-  process.exit();
-};
+/**
+ * Do basic checks to see if we need to run at all
+ */
+const path = which.sync('duplicacy', { nothrow: true });
 
+if (!path) {
+  utils.error('Duplicacy is not installed');
+}
+
+/**
+ * Good to go, run a locked backup
+ */
 lockfile.run(async () => {
-  if (!repository) {
-    error('No repository path provided as first argument');
-  }
+  program
+    .name('backup')
+    .version(packageJson.version)
+    .option(
+      '-r, --repository <path>',
+      'Path to the duplicacy repository you wish to back up',
+    )
+    .option('-l, --log', 'Enable log-style output')
+    .option(
+      '--prune-days <number>',
+      'Amount of days to keep backed up, snapshots falling outside the amount of days provided will be deleted',
+    )
+    .option('-t, --threads <number>', 'Number of uploading threads')
+    .option('-s, --stats', 'Show statistics during and after backup')
+    .parse(process.argv);
 
-  const path = which.sync('duplicacy', { nothrow: true });
-
-  if (!path) {
-    error('Duplicacy is not installed');
+  if (!program.repository) {
+    utils.error('No repository path provided to --repository flag');
   }
 
   /**
    * Create the post-backup script and make it executable
    */
-  await fs.promises.mkdir(`${repository}/.duplicacy/scripts`, {
+  await fs.promises.mkdir(`${program.repository}/.duplicacy/scripts`, {
     recursive: true,
   });
   await fs.promises.writeFile(
-    `${repository}/.duplicacy/scripts/post-backup`,
-    `#!/bin/sh\n${path} prune -keep 0:1`,
+    `${program.repository}/.duplicacy/scripts/post-backup`,
+    `#!/bin/sh\n${path} prune -keep 0:${program.pruneDays}`,
   );
   await fs.promises.chmod(
-    `${repository}/.duplicacy/scripts/post-backup`,
+    `${program.repository}/.duplicacy/scripts/post-backup`,
     '755',
   );
 
-  await duplicacy.backup(repository, {
-    log: true,
-    threads: 2,
-    stats: true,
+  await duplicacy.backup(program.repository, {
+    log: program.log ?? true,
+    threads: program.threads ?? 2,
+    stats: program.stats ?? true,
   });
 });
