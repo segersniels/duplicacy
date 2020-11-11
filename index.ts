f60f4277797dfb00b4463316a59ea368b5691171
segersniels <segers.n@hotmail.com>
2020-11-11 23:58:01 +0100

    :ambulance: Don't use __filename as base for lock file since pkg doesn't know how to handle it

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ import { program } from 'commander';
 import packageJson from 'package';
 import lockfile from 'lockfile';
 
-const filename = `${__filename}.lock`;
 const duplicacy = new Duplicacy();
 
 program
@@ -28,56 +27,60 @@ program
   .option('--dry-run', "Dry run for testing, don't backup anything")
   .parse(process.argv);
 
+if (!program.repository) {
+  utils.error('No repository path provided to --repository flag');
+}
+
 /**
  * Lock the file for 5 minutes
  */
-lockfile.lock(filename, { wait: 1000 * 60 * 5 }, (err) => {
-  if (err) {
-    utils.error(err.message);
-  }
+lockfile.lock(
+  `${program.repository}/.duplicacy/.lock`,
+  { wait: 1000 * 60 * 5 },
+  (err) => {
+    if (err) {
+      utils.error(err.message);
+    }
 
-  /**
-   * Do basic checks to see if we need to run at all
-   */
-  const path = which.sync('duplicacy', { nothrow: true });
+    /**
+     * Do basic checks to see if we need to run at all
+     */
+    const path = which.sync('duplicacy', { nothrow: true });
 
-  if (!path) {
-    utils.error('Duplicacy is not installed');
-  }
+    if (!path) {
+      utils.error('Duplicacy is not installed');
+    }
 
-  if (!program.repository) {
-    utils.error('No repository path provided to --repository flag');
-  }
+    /**
+     * Create the post-backup script and make it executable
+     */
+    fs.mkdirSync(`${program.repository}/.duplicacy/scripts`, {
+      recursive: true,
+    });
+    fs.writeFileSync(
+      `${program.repository}/.duplicacy/scripts/post-backup`,
+      `#!/bin/sh\n${path} prune -keep 0:${program.pruneDays ?? 1}`,
+    );
+    fs.chmodSync(`${program.repository}/.duplicacy/scripts/post-backup`, '755');
 
-  /**
-   * Create the post-backup script and make it executable
-   */
-  fs.mkdirSync(`${program.repository}/.duplicacy/scripts`, {
-    recursive: true,
-  });
-  fs.writeFileSync(
-    `${program.repository}/.duplicacy/scripts/post-backup`,
-    `#!/bin/sh\n${path} prune -keep 0:${program.pruneDays ?? 1}`,
-  );
-  fs.chmodSync(`${program.repository}/.duplicacy/scripts/post-backup`, '755');
-
-  duplicacy.backup(
-    program.repository,
-    {
-      log: program.log ?? true,
-      threads: program.threads ?? 2,
-      stats: program.stats ?? true,
-      ['dry-run']: program.dryRun ?? false,
-    },
-    () => {
-      /**
-       * Unlock the file after backup has been completed
-       */
-      lockfile.unlock(filename, (err) => {
-        if (err) {
-          utils.error(err.message);
-        }
-      });
-    },
-  );
-});
+    duplicacy.backup(
+      program.repository,
+      {
+        log: program.log ?? true,
+        threads: program.threads ?? 2,
+        stats: program.stats ?? true,
+        ['dry-run']: program.dryRun ?? false,
+      },
+      () => {
+        /**
+         * Unlock the file after backup has been completed
+         */
+        lockfile.unlock(`${program.repository}/.duplicacy/.lock`, (err) => {
+          if (err) {
+            utils.error(err.message);
+          }
+        });
+      },
+    );
+  },
+);
